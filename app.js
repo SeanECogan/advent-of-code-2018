2e22f333a15bbb1e11062c6e9c6acd8a658325d4
Sean Cogan <sean.e.cogan@gmail.com>
2018-12-01 11:24:34 -0500

    Set up top-level project structure.

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function(){
+    let startTime = new Date();
+
+    let endTime = new Date();
+
+    let timeElapsed = endTime - startTime;
+
+    console.log(`Puzzle solved in ${timeElapsed}.`);
+})();
